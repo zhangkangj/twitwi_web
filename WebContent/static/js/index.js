6167a4608031f7d724bc9ed65ae50c573d5cb453
Xunjie Li <xunjieli@gmail.com>
2012-09-12 12:04:29 -0400

    added carousel and topic graph with fake data

--- WebContent/static/js/index.js
+++ WebContent/static/js/index.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 		// set up date selector
 		d3.json("/static/dat/times.json", function(json) {
 			var times = json.times.reverse();
-			var ts = d3.select("#map_container").append("div").append("select");
+			var ts = d3.select("#page").append("div").append("select");
 			// compensate the EST timezone offset
 			ts.selectAll("option").data(times).enter().append("option").attr("value", String).text(function(d) {return (new Date((d + 4*3600)*1000)).toDateString();});
 			// initialize with the lastest data
@@ -33,7 +33,7 @@ $(document).ready(function() {
 				var temp = item.obama/(item.obama+item.romney);
 				if (temp > 0.5) temp = 0.5 + Math.sqrt((temp - 0.5)*2)/2;
 				else temp = 0.5 - Math.sqrt((0.5 - temp)*2)/2;
-				console.log(temp);
+//				console.log(temp);
 				g.select("#"+item.state).select("path").style("fill",color(temp));
 				//append("svg:title").text(item.state+": Obama: "+d3.round(item.obama/(item.obama+item.romney)*100)+"%, Romney: "+d3.round(item.romney/(item.obama+item.romney)*100)+"%");
 			}
@@ -66,4 +66,119 @@ $(document).ready(function() {
 		}
 		g.selectAll("g.state").classed("hover", over && function(d) { return d == over; });
 	}
+
+  // topic graph
+	var w = 1000,
+	    h = 600,
+	    node,
+	    link,
+	    root;
+	var force = d3.layout.force()
+	    .on("tick", tick)
+	    .charge(function(d) { return d._children ? -d.size / 100 : -30; })
+	    .linkDistance(function(d) { return d.target._children ? 80 : 30; })
+	    .size([w, h - 160]);
+
+	var vis = d3.select("#topic_container").append("svg:svg")
+	    .attr("width", w)
+	    .attr("height", h);
+
+	d3.json("static/js/flare.json", function(json) {
+	  root = json;
+	  root.fixed = true;
+	  root.x = w / 2;
+	  root.y = h / 2 - 80;
+	  update();
+	});
+
+	function update() {
+	  var nodes = flatten(root),
+	      links = d3.layout.tree().links(nodes);
+
+	  // Restart the force layout.
+	  force
+	      .nodes(nodes)
+	      .links(links)
+	      .start();
+
+	  // Update the links…
+	  link = vis.selectAll("line.link")
+	      .data(links, function(d) { return d.target.id; });
+
+	  // Enter any new links.
+	  link.enter().insert("svg:line", ".node")
+	      .attr("class", "link")
+	      .attr("x1", function(d) { return d.source.x; })
+	      .attr("y1", function(d) { return d.source.y; })
+	      .attr("x2", function(d) { return d.target.x; })
+	      .attr("y2", function(d) { return d.target.y; });
+
+	  // Exit any old links.
+	  link.exit().remove();
+
+	  // Update the nodes…
+	  node = vis.selectAll("circle.node")
+	      .data(nodes, function(d) { return d.id; })
+	      .style("fill", color);
+
+	  node.transition()
+	      .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.size) / 10; });
+
+	  // Enter any new nodes.
+	  node.enter().append("svg:circle")
+	      .attr("class", "node")
+	      .attr("cx", function(d) { return d.x; })
+	      .attr("cy", function(d) { return d.y; })
+	      .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.size) / 10; })
+	      .style("fill", color)
+	      .on("click", click)
+	      .call(force.drag);
+
+	  // Exit any old nodes.
+	  node.exit().remove();
+	}
+
+	function tick() {
+	  link.attr("x1", function(d) { return d.source.x; })
+	      .attr("y1", function(d) { return d.source.y; })
+	      .attr("x2", function(d) { return d.target.x; })
+	      .attr("y2", function(d) { return d.target.y; });
+
+	  node.attr("cx", function(d) { return d.x; })
+	      .attr("cy", function(d) { return d.y; });
+	}
+
+	// Color leaf nodes orange, and packages white or blue.
+	function color(d) {
+	  return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
+	}
+
+	// Toggle children on click.
+	function click(d) {
+	  if (d.children) {
+	    d._children = d.children;
+	    d.children = null;
+	  } else {
+	    d.children = d._children;
+	    d._children = null;
+	  }
+	  update();
+	}
+
+	// Returns a list of all nodes under the root.
+	function flatten(root) {
+	  var nodes = [], i = 0;
+
+	  function recurse(node) {
+	    if (node.children) node.size = node.children.reduce(function(p, v) { return p + recurse(v); }, 0);
+	    if (!node.id) node.id = ++i;
+	    nodes.push(node);
+	    return node.size;
+	  }
+
+	  root.size = recurse(root);
+	  return nodes;
+	}
 });
+
+

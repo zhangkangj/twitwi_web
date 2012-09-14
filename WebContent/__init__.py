072fcade08bfcdd0c1e76a08367cbf5a3ef0bd40
zhk <zhk@mit.edu>
2012-09-14 13:25:20 -0400

    day -> time

--- WebContent/__init__.py
+++ WebContent/__init__.py
@@ -27,12 +27,58 @@ def count():
         return json.dumps('data',[])
     result = []
     cursor = g.con.cursor()
-    cursor.execute("""SELECT * FROM election where day = %s """, (time,))
+    cursor.execute("""SELECT * FROM election where time = %s """, (time,))
     entry = cursor.fetchone()
     while entry:
         result.append({'state':entry[1],'obama':entry[2],'romney':entry[3]})
         entry = cursor.fetchone()
     return json.dumps({'data':result})
+
+@app.route('/mention')
+def mention():
+    result = {}
+    cursor = g.con.cursor()
+    cursor.execute("""SELECT time, state, entity, count FROM election_count WHERE topic = 'mention' ORDER BY time""")
+    row = cursor.fetchone()
+    while row:
+        time = row[0]
+        state = row[1]
+        entity = row[2]
+        count = row[3]
+        if time not in result:
+            result[time] = {}
+        entity_dict = result[time]
+        if entity not in entity_dict:
+            entity_dict[entity] = {}
+        state_dict = entity_dict[entity]
+        if state not in state_dict:
+            state_dict[state] = {}
+        state_dict[state] = count
+        row = cursor.fetchone()
+    return json.dumps(result)
+    
+@app.route('/topic')
+def topic():
+    result = {}
+    cursor = g.con.cursor()
+    cursor.execute("""SELECT time, topic, entity, count FROM election_count WHERE topic != 'mention' ORDER BY time""")
+    row = cursor.fetchone()
+    while row:
+        time = row[0]
+        topic = row[1]
+        entity = row[2]
+        count = row[3]
+        if time not in result:
+            result[time] = {}
+        entity_dict = result[time]
+        if entity not in entity_dict:
+            entity_dict[entity] = {}
+        topic_dict = entity_dict[entity]
+        if topic not in topic_dict:
+            topic_dict[topic] = {}
+        topic_dict[topic] = count
+        row = cursor.fetchone()
+    return json.dumps(result)
 
 @app.route('/sample')
 def sample_tweet():

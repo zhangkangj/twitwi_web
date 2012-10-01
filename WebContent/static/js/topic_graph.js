ebe6f41d9048fc4c5bdea9b3a4a9c8e183a2938a
Xunjie Li <xunjieli@gmail.com>
2012-09-30 20:49:25 -0400

    changed bubble chart to line chart

--- WebContent/static/js/topic_graph.js
+++ WebContent/static/js/topic_graph.js
@@ -5,12 +5,12 @@ function topic_graph(){
   vis_left = d3.select("#topic_left").append("svg")
       .attr("width", width)
       .attr("height", height)
-      .attr("class", "pack")
+      .attr("id", "top_rect")
       .append("g");
   vis_right = d3.select("#topic_right").append("svg")
       .attr("width", width)
       .attr("height", height)
-      .attr("class", "pack")
+      .attr("id", "bottom_rect")
       .append("g");
 
 }
@@ -30,7 +30,7 @@ function update_topic(json) {
 
     var sum_array = [];
     for (var c in topicJson1) {
-      sum_array.push(Math.sqrt(topicJson1[c]/sum_1)*10);
+      sum_array.push(Math.sqrt(topicJson1[c]/sum_1));
     }
 
     for (var k = 0; k < sum_array.length; k++){
@@ -39,7 +39,7 @@ function update_topic(json) {
 
     var i = 0;
     for (var c in topicJson1) {
-      topicArray1.push({"name": c, "size": (sum_array[i]/sum_2)*400, "order": i});
+      topicArray1.push({"name": c, "size": (sum_array[i]/sum_2)*800});
       i++;
     }
 
@@ -51,7 +51,7 @@ function update_topic(json) {
 
     sum_array = [];
     for (var c in topicJson2) {
-      sum_array.push(Math.sqrt(topicJson2[c]/sum_1)*10);
+      sum_array.push(Math.sqrt(topicJson2[c]/sum_1));
     }
 
     for (var k = 0; k < sum_array.length; k++){
@@ -60,50 +60,63 @@ function update_topic(json) {
 
     i = 0;
     for (var c in topicJson2) {
-      topicArray2.push({"name": c, "size": (sum_array[i]/sum_2)*400, "order": i});
+      topicArray2.push({"name": c, "size": (sum_array[i]/sum_2)*800});
       i++;
     }
+
+    topicArray1 = topicArray1.sort(sortingFunc);
+    topicArray2 = topicArray2.sort(sortingFunc);
     var node_left = vis_left.selectAll("g.node")
           .data(topicArray1);
 
-     var radius_count = 0;
+     var width_count = 0;
      var left_enter =  node_left.enter().append("g");
 
      node_left.attr("class", function(d) { return d.children ? "node" : "dem node"; })
-       .attr("transform", function(d) {val = radius_count + d.size; radius_count += 2*d.size;return "translate(" + val + "," + 100 + ")"; })
-     left_enter.append("circle");
-    var left_c =  node_left.select("circle")
-          .attr("r", function(d) { return d.size; })
+       .attr("transform", function(d) {val = width_count; width_count += d.size;return "translate(" + val + "," + 30 + ")"; })
+     left_enter.append("rect");
+    var left_c =  node_left.select("rect")
+          .attr("width", function(d) { return d.size; })
+          .attr("height", function(d) { return 100;})
           .attr("id", function(d) { return d.children ? "dem" : "leaf";});
      left_c.filter(function(d) { return !d.children; })
           .style("fill", function(d) { return fill(d.name);});
     left_enter.filter(function(d) { return !d.children; }).append("text");
     node_left.select("text")
           .attr("text-anchor", "middle")
-          .attr("dy", ".3em")
-          .text(function(d) { return d.name.substring(0, d.size / 3); });
+          .attr("transform", function(d) {return "translate(" + d.size/2 + "," + 50 + ")";})
+          .text(function(d) { return d.name.substring(0, d.size / 9); });
 
       node_left.exit().remove();
 
     var node_right = vis_right.selectAll("g.node").data(topicArray2);
-     radius_count = 0;
+     width_count = 0;
      var right_enter =  node_right.enter().append("g");
 
      node_right.attr("class", function(d) { return d.children ? "node" : "dem node"; })
-       .attr("transform", function(d) {val = radius_count + d.size; radius_count += 2*d.size;return "translate(" + val + "," + 100 + ")"; })
-     right_enter.append("circle");
-    var right_c =  node_right.select("circle")
-          .attr("r", function(d) { return d.size; })
+       .attr("transform", function(d) {val = width_count; width_count += d.size;return "translate(" + val + "," + 0 + ")"; })
+     right_enter.append("rect");
+    var right_c =  node_right.select("rect")
+          .attr("width", function(d) { return d.size; })
+          .attr("height", function(d) { return 100;})
           .attr("id", function(d) { return d.children ? "dem" : "leaf";});
-     right_c.filter(function(d) { return !d.children; })
-          .style("fill", function(d) { return fill(d.name);});
-    right_enter.filter(function(d) { return !d.children; }).append("text");
+    right_c.style("fill", function(d) { return fill(d.name);});
+
+    right_enter.append("text");
     node_right.select("text")
           .attr("text-anchor", "middle")
-          .attr("dy", ".3em")
-          .text(function(d) { return d.name.substring(0, d.size / 3); });
+          .attr("transform", function(d) {return "translate(" + d.size/2 + "," + 50 + ")";})
+          .text(function(d) { return d.name.substring(0, d.size / 9); });
 
       node_right.exit().remove();
 }
 
-
+function sortingFunc(a,b) {
+var nA = a.name.toLowerCase();
+   var nB = b.name.toLowerCase();
+     if(nA < nB)
+           return -1;
+       else if(nA > nB)
+             return 1;
+        return 0;
+}

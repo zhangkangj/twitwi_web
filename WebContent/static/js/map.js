86e85b5cb54cd6fe340a9a89caf1b9078049555d
zhk <zhk@mit.edu>
2012-10-13 21:38:53 -0400

    full legend

--- WebContent/static/js/map.js
+++ WebContent/static/js/map.js
@@ -24,8 +24,8 @@ function setup_map() {
 	var gradient_width=40, gradient_height=10, gradient_count=gradient.length;
 	var legend = svg.append('g').attr('id', 'legend').attr('transform', 'translate('+(width/2-gradient_width*gradient_count/2)+','+(height-gradient_height-10)+')');
 	legend.selectAll('rect').data(gradient).enter().append('rect').attr('class', 'gradient').attr('width', gradient_width).attr('height', gradient_height).attr('fill', function(d) { return d; }).attr('x', function(d, k) {return k*gradient_width;});
-	legend.append('text').text('Romney').attr('x', -65).attr('y', 10);
-	legend.append('text').text('Obama').attr('x', gradient_width*gradient_count+10).attr('y', 10);
+	legend.append('text').text('More Romney Mention').attr('x', -150).attr('y', 10);
+	legend.append('text').text('More Obama Mention').attr('x', gradient_width*gradient_count+10).attr('y', 10);
 	
 	$(c).append(box);
 }
